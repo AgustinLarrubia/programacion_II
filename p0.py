92b6d5d7c0956d009becb45e5b48bf2afe6171e3
Agustin Larrubia <agustinlarrubia@gmail.com>
2025-08-30 18:57:58 +0000

    Se agregaron ejercicios 2, 3 y 4

--- p0.py
+++ p0.py
@@ -39,7 +39,51 @@ def repite_hola_concat(n: int) -> str:
     
     return "Hola" + repite_hola_concat(n - 1)
 
-print(repite_hola_concat(4))
+#print(repite_hola_concat(4))
 
 # Ejercicio 2
 
+def fibonacci(n: int) -> int: 
+
+    if(n == 0):
+        return 0
+    if(n == 1):
+        return 1
+
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+#print(fibonacci(5))
+
+# Ejercicio 3
+
+def print_string(n: int, s: str) -> None:
+    if(n < 1):
+        return None
+    
+    print(s)
+    return print_string(n - 1, s)
+
+#print_string(1, "Hola")
+
+
+# Ejercicio 4
+
+""" Convierta la siguiente función iterativa a una definición recursiva. """
+
+def iterativa(l: list[int]) -> int:
+    c = 1
+    for i in l:
+        c = c * i
+    return c
+
+
+
+def recursiva(l: list[int]) -> int:
+
+    if(not l):
+        return 1
+    
+    return l[0] * recursiva(l[1:])
+
+#print(iterativa([1,4,5,3,1]))
+#print(recursiva([1,4,5,3,1]))
