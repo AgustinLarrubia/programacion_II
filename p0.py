d71b9b1348840d4addfe8c2ca3a1987476022306
AgustinLarrubia <agustinlarrubia@gmail.com>
2025-08-31 21:25:57 +0000

    Se agregaron los ejercicios 5, 6, 7, 8, 9 y 10 ademas se completo el ejercicio 3 que me me falto una funcion y se actualizo el pdf ya que la practica que subi era vieja

--- p0.py
+++ p0.py
@@ -63,7 +63,15 @@ def print_string(n: int, s: str) -> None:
     print(s)
     return print_string(n - 1, s)
 
-#print_string(1, "Hola")
+def repeat_string(n: int, s: str) -> str:
+
+    if(n == 0):
+        return ''
+    
+    return s + repeat_string(n - 1, s)
+
+#print_string(4, "Hola")
+#print(repeat_string(4, "Test"))
 
 
 # Ejercicio 4
@@ -87,3 +95,86 @@ def recursiva(l: list[int]) -> int:
 
 #print(iterativa([1,4,5,3,1]))
 #print(recursiva([1,4,5,3,1]))
+
+# Ejercicio 5
+
+def find_max(intList: list[int]) -> int:
+    
+    if(len(intList) == 1):
+        return intList[0]
+    
+    if(intList[0] > intList[1]):
+        intList[1] = intList[0]
+
+    return find_max(intList[1:])
+
+#print(find_max([2,4,211,34,23,42,21,1,4]))
+
+# Ejercicio 6
+
+def power(a: int, b: int) -> int:
+
+    if(b == 0):
+        return 1 
+    
+    return a * power(a, b - 1)
+
+#print(power(5,5))
+
+# Ejercicio 7
+
+def count_digits(num: int) -> int:
+
+    cociente = num // 10
+    
+    if(cociente == 0):
+        return 1
+    
+    return 1 + count_digits(cociente)
+
+#print(count_digits(0))
+
+# Ejercicio 8
+
+def reverse_string_iterative(s: str) -> str:
+
+    reversed_string = ""
+
+    for i in range(len(s), 0, -1):
+         reversed_string = reversed_string + s[i - 1]
+    
+    return reversed_string
+
+
+def reverse_string_recursive(s: str) -> str:
+
+    if(len(s) == 1):
+        return s[0]
+    
+    return s[-1] + reverse_string_recursive(s[:-1])
+
+
+#print(reverse_string_recursive("Agustin"))
+#print(reverse_string_iterative("Agustin"))
+
+# Ejercicio 9
+
+def replicate(l: list[int], n: int) -> list[int]:
+
+    if(not l):
+        return []
+    
+    return [l[0]] * n + replicate(l[1:], n)
+
+#print(replicate([1,2,2,4,2],3)) 
+
+# Ejercicio 10 
+
+def is_palindrome(s: str) -> bool:
+
+    if(s == ''):
+        return True
+    return s[0].lower() == s[-1].lower() and is_palindrome(s[1:-1])   
+
+#print(is_palindrome("Neuquen"))
+
