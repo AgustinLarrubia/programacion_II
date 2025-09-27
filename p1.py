8a06326503fdefedcb1100cde439a2581bb5c7af
AgustinLarrubia <agustinlarrubia@gmail.com>
2025-09-27 22:36:55 +0000

    Se agrego ejercicio 4 y 5

--- p1.py
+++ p1.py
@@ -120,7 +120,7 @@ print(auto)
 auto.avanzar(1)
 print(auto) """
 
-auto = Automovil("AEF-202", "Peugeot")
+""" auto = Automovil("AEF-202", "Peugeot")
 auto.cargar_nafta(10)
 print(auto.km_recorridos)  # 0
 print(auto.litros_nafta)   # 10
@@ -133,4 +133,121 @@ auto.avanzar(100)          # Es necesario cargar nafta para recorrer la cantidad
 
 auto.avanzar(40)
 print(auto.km_recorridos)  # 90
-print(auto.litros_nafta)   # 2.08
+print(auto.litros_nafta)   # 2.08 """
+
+# Ejercicio 4
+
+# Robot que se mueve en plano cartesiano x y infinito
+# Movimientos
+#  - Avanzar (A)
+#  - Retroceder  (R)
+#  - Avanzar hacia la izquierda (I) o la derecha (D)
+# Metodos
+# Mueve(orden (Movimientos)), posicion_actual(Coordenada))
+# El robot se inicializara en (0,0)
+
+# Ejercicio 5
+# Ahora mover puede recibir uno o una secuencia de movimientos. Ejemplo: "A" o "RRAARDDI"
+# ATENCION: si la secuencia de movimientos contiene un movimiento invalido, debe informarlo antes de realizar cualquier movimiento
+# Agregar metodo obtener_historico_de_movimientos que devuelva el historial de movimientos que realizo el robot
+# Agregar un metodo como_volver que indique la secuencia de movimientos necesarios para volver al origen ( (0,0) )
+
+class Robot():
+
+    def __init__(self) -> None:
+        self.x: float = 0
+        self.y: float = 0
+        self.movimientos: dict[str , tuple[float, float]] = {
+            'A': (0,1),
+            'R': (0,-1),
+            'I': (-1,0),
+            'D': (1,0)
+        }
+        self.historial: str = ""
+
+    def mover(self, movimiento: tuple[float, float]) -> None:
+        dx, dy = movimiento
+        self.x += dx
+        self.y += dy
+
+    def posicion_actual(self) -> tuple[float, float]:
+
+        return (self.x, self.y)
+    
+    
+    def obtener_historico_de_movimientos(self) -> str:
+
+        return self.historial
+    
+    def registrar_historico(self, orden: str) -> str:
+        self.historial += orden
+        return self.historial
+    
+
+    def limpiar_historial(self) -> None:
+        self.historial = ""
+
+    def invertir_movimiento(self, orden: str) -> str:
+        opuestos = {
+            'A': 'R',
+            'R': 'A',
+            'D': 'I',
+            'I': 'D'
+        }
+        return opuestos[orden]
+
+    def como_volver(self) -> str:
+        ordenes_volver = ""
+        for orden in reversed(self.historial):
+            ordenes_volver += self.invertir_movimiento(orden)
+
+        self.limpiar_historial()
+
+        return ordenes_volver
+    
+    def check_ordenes(self, ordenes: str) -> tuple[str, str]:
+
+        ordenes_validas: str = "".join(char for char in ordenes if char in self.movimientos)
+        ordenes_invalidas: str = "".join(char for char in ordenes if char not in self.movimientos)
+
+        if(len(ordenes_invalidas) > 0):
+            print(f"Se encontraron ordenes no validas {ordenes_invalidas}. El programa continuara su ejecucion")
+
+        return ordenes_validas, ordenes_invalidas
+
+    def mueve(self, ordenes: str) -> None :
+
+        ordenes = ordenes.upper() # Formateo las ordenes para que todas esten en mayus
+
+        ordenes_validas, ordenes_invalidas = self.check_ordenes(ordenes)
+
+        for orden in ordenes_validas:
+            movimiento: tuple[float, float] = self.movimientos[orden]
+            self.mover(movimiento)
+            self.registrar_historico(orden)
+        
+        return self.posicion_actual()
+
+
+# TEST Ejercicio 4
+""" mi_robot = Robot()
+orden = input("Introduce la orden: ")
+while orden != 'fin':
+    mi_robot.mueve(orden)
+    print(mi_robot.posicion_actual())
+    orden = input("Introduce la orden: ") """
+
+# TEST Ejercicio 5
+mi_robot = Robot()
+orden = input("Introduce la orden, volver para regresar al origen y fin para terminar: ")
+while orden != 'fin':
+
+    if(orden.upper() == 'VOLVER'):
+        orden = mi_robot.como_volver()
+    
+    print(orden)
+    mi_robot.mueve(orden)
+    print(mi_robot.posicion_actual())
+    orden = input("Introduce la orden: ")
+
+print(mi_robot.obtener_historico_de_movimientos())
