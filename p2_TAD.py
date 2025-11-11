9ab8685797555ff408d1fe395ef29eb145726d64
AgustinLarrubia <agustinlarrubia@gmail.com>
2025-11-11 01:07:45 +0000

    Se empezo a hacer el ejercicio 6

--- p2_TAD.py
+++ p2_TAD.py
@@ -1,3 +1,4 @@
+from hmac import new
 from typing import Any
 
 class _Node:
@@ -366,7 +367,7 @@ print(linkedList1) """
 
 # Ejercicio 5
 
-linkedList1 = LinkedList()
+""" linkedList1 = LinkedList()
 linkedList1.insert(0, 1)
 linkedList1.insert(0, 1)
 linkedList1.insert(0, 3)
@@ -382,11 +383,137 @@ linkedList1.insert(0, 1)
 
 print(linkedList1)
 linkedList1.invert()
-print(linkedList1)
+print(linkedList1) """
 
+# Ejercicio 6
+
+class _DoubleNode:
+
+    def __init__(self, data: Any = None, next: '_DoubleNode' = None, prev: '_DoubleNode' = None) -> None:
+
+        self.data: Any = data
+        self.next: _DoubleNode = next
+        self.prev: _DoubleNode = prev
+
+    def __str__(self) -> str:
+
+        return str(self.data)
+    
+
+        
+
+class DoublyLinkedList:
+
+    def __init__(self) -> None:
+
+        self.first: _DoubleNode | None = None
+        self.last: _DoubleNode | None = None
+        self.len = 0
+
+    def __str__(self) -> str:
+        
+        n_act = self.first
+
+        if(n_act is None):
+            return str(None)
+        
+        linkedList: str = ''
+        
+        while n_act is not None:
+            linkedList += f"{n_act.data}, " if n_act.next is not None else f"{n_act.data}."
+            n_act = n_act.next
+
+        return linkedList
+
+
+
+    def __len__(self) -> int:
+        return self.len
+
+    def append(self, x: Any) -> None:
+
+        new_node = _DoubleNode(x)
+
+        if(self.first is None and self.last is None):
+
+            self.first = new_node
+
+            self.last = new_node
+        else:
+            self.last.next = new_node
+
+            new_node.prev = self.last
+            self.last = new_node
+
+        self.len += 1
+            
+
+    def insert(self, i: int, x: Any) -> None:
+
+        if(i < 0 or i > self.len):
+            print("Posicion Invalida.")
+            return
+        
+        new_node = _DoubleNode(x)
+
+        if(i == 0):
+
+            new_node.next = self.first
+            self.first.prev = new_node
+            self.first = new_node
+
+        elif(i == self.len):
+
+            self.last.next = new_node
+            new_node.prev = self.last
+            self.last = new_node
+        
+        else:
+
+            n_prev = self.first
+
+            for _ in range(1, i):
+
+                n_prev = n_prev.next
+
+            new_node.prev = n_prev
+            new_node.next = n_prev.next
+            n_prev.next.prev = new_node
+            n_prev.next = new_node
+
+        self.len += 1
+
+
+            
+            
+            
+
+
+
+
+            
+            
+
+    def pop(self, x) -> Any:
+        pass
 
 # Implementar raise ValueError
 
+
+doubleLinkedList = DoublyLinkedList()
+
+
+doubleLinkedList.append(1)
+doubleLinkedList.append(2)
+doubleLinkedList.append(5)
+doubleLinkedList.append(79)
+
+
+#print(doubleLinkedList)
+
+doubleLinkedList.insert(3, 7)
+
+print(doubleLinkedList)
 
 class Stack:
     """ 
